b71ed31eff3692cdd41da3bfe4f90edf0e4bfd70
Vojtěch Dubrovský <v.dubr@hotmail.com>
2020-03-16 12:28:57 +0100

    Build package by rollup

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-import {forIn} from 'lodash';
+import _ from 'lodash';
 import i18n from './i18n';
 
 export default {
     addI18nResources(namespace, resources){
-        forIn(resources, (resource, language) => {
+        _.forIn(resources, (resource, language) => {
             i18n.addResourceBundle(language, namespace, resource, true, true);
         });
     }
