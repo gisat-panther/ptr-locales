47837ff2faa292b34893645a2b687923b086c610
vdubr <vdubr@users.noreply.github.com>
2021-06-21 07:54:36 +0000

    Fix code style e93c9dd74c7ec9f7c29500daacafb0e9e5b9df9f [ci skip]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import localesUtils from './utils';
 import i18n from './i18n';
 import {withTranslation, initReactI18next} from 'react-i18next';
 
-export {i18n, localesUtils, withTranslation as withNamespaces, initReactI18next};
+export {
+	i18n,
+	localesUtils,
+	withTranslation as withNamespaces,
+	initReactI18next,
+};
 
 export default {
 	i18n,
