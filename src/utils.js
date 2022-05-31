88c516791eb8b12a7f51cee76615b175c4377957
Vojtěch Dubrovský <v.dubr@hotmail.com>
2022-05-31 14:02:24 +0200

    Add eslint

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,6 @@ import i18n from './i18n';
 
 export default {
 	addI18nResources(namespace, resources) {
-		var a = {};
 		_.forIn(resources, (resource, language) => {
 			i18n.addResourceBundle(language, namespace, resource, true, true);
 		});
